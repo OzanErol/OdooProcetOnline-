702b84a55f6dd3096c90112a6d790586665eab3a
AyseTurk <ayse69turk@gmail.com>
2020-01-05 15:41:40 -0600

    added locator

--- src/test/java/com/Odoo/pages/BasePage.java
+++ src/test/java/com/Odoo/pages/BasePage.java
@@ -1,9 +1,16 @@
 package com.Odoo.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class BasePage {
 
     public static void main(String[] args) {
 
+        @FindBy(xpath="//div[@class='navbar-collapse collapse'] /ul[1]/li")
+          public List<WebElement> moduleElement;
         public BasePage() {
             //this method requires to provide webdriver object for @FindBy
             //and page class
