7147b321efe2211b06a3286784bd17118bb8a065
AyseTurk <ayse69turk@gmail.com>
2020-01-04 20:04:03 -0600

    added BasePage constructor

--- src/test/java/com/Odoo/pages/BasePage.java
+++ src/test/java/com/Odoo/pages/BasePage.java
@@ -4,5 +4,12 @@ public class BasePage {
     public static void main(String[] args) {
         System.out.println("Why it is not working");
         //dasdaadgit
+
+        public BasePage() {
+            //this method requires to provide webdriver object for @FindBy
+            //and page class
+            //this means this page class
+            PageFactory.initElements(Driver.get(), this);
+        }
     }
 }
