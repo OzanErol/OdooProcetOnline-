4371e3cea22c685101c26ee0d68af16ad5e89e69
Aysegul Yigit <yigit.aysegul@icloud.com>
2020-01-04 19:52:01 -0600

    1/4/19

--- src/test/java/com/Odoo/pages/BasePage.java
+++ src/test/java/com/Odoo/pages/BasePage.java
@@ -3,6 +3,7 @@ package com.Odoo.pages;
 public class BasePage {
     public static void main(String[] args) {
         System.out.println("Why it is not working");
-        //dasdaadgit
+        //
+
     }
 }
