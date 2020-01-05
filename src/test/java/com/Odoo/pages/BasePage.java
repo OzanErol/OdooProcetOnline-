903550dcf7b00cb8a85b4dcfca04315368dbc597
OzanErol <erolozan96@gmail.com>
2020-01-04 20:07:50 -0600

    Seven try

--- src/test/java/com/Odoo/pages/BasePage.java
+++
@@ -1,8 +0,0 @@
-package com.Odoo.pages;
-
-public class BasePage {
-    public static void main(String[] args) {
-        System.out.println("Why it is not working");
-        //dasdaadgit
-    }
-}
